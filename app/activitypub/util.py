b1e9f450f12b92351e763736ba0869def7ff0fa9
karasugawasu <hotaruchan.saturn@gmail.com>
2026-01-09 18:07:21 +0900

    フレアになったタグはタグにしない

--- app/activitypub/util.py
+++ app/activitypub/util.py
@@ -3062,14 +3062,17 @@ def update_post_from_activity(post: Post, request_json: dict):
                 if (not post.microblog) and (json_tag['name'][1:].lower() == post.community.name.lower()): # Lemmy adds the community slug as a hashtag on every post in the community, which we want to ignore
                     continue
                 name = json_tag.get('name')
-                hashtag = find_hashtag_or_create(name)
-                if hashtag:
-                    post.tags.append(hashtag)
+                if not isinstance(name, str):
+                    continue
                 if post.microblog:
                     flair_name = name[1:] if name.startswith('#') else name
                     flair = find_flair(flair_name, post.community_id)
                     if flair and flair not in post.flair:
                         post.flair.append(flair)
+                        continue
+                hashtag = find_hashtag_or_create(name)
+                if hashtag:
+                    post.tags.append(hashtag)
             if json_tag['type'] == 'lemmy:CommunityTag':
                 # change back when lemmy supports flairs
                 # flair = find_flair_or_create(json_tag, post.community_id)
